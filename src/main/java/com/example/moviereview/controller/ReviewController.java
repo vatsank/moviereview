36addd9f7ce72cb7cdee9cabe0ddbd50624c77de
Srivatsan <vatsank@gmail.com>
2019-11-29 12:29:26 +0530

    As per request WEBREQ-1 added method

--- src/main/java/com/example/moviereview/controller/ReviewController.java
+++ src/main/java/com/example/moviereview/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.moviereview.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,4 +14,9 @@ public class ReviewController {
         return "Hello World";
     }
 
+    @GetMapping(value = "/message/{info}")
+    public String showMessage(@PathVariable("info") String msg){
+
+        return "Added One More Method";
+    }
 }
