c38025487572fe334d23636d7ce816ed5116043f
Srivatsan <vatsank@gmail.com>
2019-11-29 12:24:00 +0530

    Initial commit

---
+++ src/main/java/com/example/moviereview/controller/ReviewController.java
@@ -0,0 +1,16 @@
+package com.example.moviereview.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewController {
+
+
+    @GetMapping(value="/message")
+    public String getMessage(){
+
+        return "Hello World";
+    }
+
+}
